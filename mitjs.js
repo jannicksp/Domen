2eb5b2858474b4d0d4aa11e6d99036076e5bda67
jannicksp <jannick_sp@live.dk>
2019-05-21 22:12:50 +0200

    domen

---
+++ mitjs.js
@@ -0,0 +1,36 @@
+function erSynlig(elementId){
+    var elementBoks = document.getElementById(elementId).getBoundingClientRect();
+    var halvtredsPct = elementBoks.height*0.5;
+    var start = window. innerHeight-halvtredsPct;
+    if(elementBoks.top <= start && elementBoks.bottom -halvtredsPct > 0 ){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+
+function aktiverMultimedier(elementIdListe, AVIdListe){
+    for (var i=0; i < elementIdListe.length; i++){
+        if (erSynlig(elementIdListe[i])){
+          document.getElementById(AVIdListe[i]).play();  
+        }
+        else{
+            document.getElementById(AVIdListe[i]).pause();
+        }
+    }
+    
+}
+
+
+//hovedprogramdel
+var elementIdListe =["paris","istanbul","firenze","video"];
+var AVIdListe =["bouncy", "walker","funky","video"];
+
+window.addEventListener("scroll", function(){
+    aktiverMultimedier (elementIdListe,AVIdListe);
+});
+
+document. getElementById("tilVideo").addEventListener("click",function () {
+    document. getElementById("video").scrollIntoView(true);
+});
