51007d3eb42191320be66138975b24cced3c194d
jannicksp <jannick_sp@live.dk>
2019-05-28 12:26:31 +0200

    js grøn pil

--- mitjs.js
+++ mitjs.js
@@ -69,6 +69,32 @@ document.getElementById("44").addEventListener("click",function() {
 
 
 
+document.getElementById("111").addEventListener("click",function() {
+    document.getElementById("222").scrollIntoView(true);
+    
+});
+document.getElementById("222").addEventListener("click",function() {
+    document.getElementById("333").scrollIntoView(true);
+});
+document.getElementById("333").addEventListener("click",function() {
+    document.getElementById("000").scrollIntoView(true);
+    
+    
+});
+
+
+
+
+document.getElementById("1111").addEventListener("click",function() {
+    document.getElementById("2222").scrollIntoView(true);
+    
+});
+document.getElementById("2222").addEventListener("click",function() {
+    document.getElementById("0000").scrollIntoView(true);
+});
+
+
+
 
 
 
